51c74554b1fd06041e9ec47a3cb75a4b5380686e
Alexandr <79134038382@mail.ru>
2025-10-03 15:33:01 +0300

    Fix-2 router base URL for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import PrivacyView from '@/views/PrivacyView.vue'
 import ErrorView from '@/views/ErrorView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/ILMax-vue3/'),
   routes: [
     {
       path: '/',
